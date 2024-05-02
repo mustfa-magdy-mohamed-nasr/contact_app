d9ec6aee2227becf207477046d67367d49de82ca
mustfa-magdy-mohamed-nasr <Mostfammaaggddyy@gmail.com>
2024-05-02 03:51:04 +0300

    createtyp adabter for contact model

--- lib/views/model/contact_model.dart
+++ lib/views/model/contact_model.dart
@@ -1,7 +1,14 @@
-class ContactModel {
+import 'package:hive/hive.dart';
+part 'contact_model.g.dart';
+@HiveType(typeId: 0)
+class ContactModel extends HiveObject {
+  @HiveField(0)
   final String name;
+  @HiveField(1)
   final String phone;
+  @HiveField(2)
   final String email;
+  @HiveField(3)
   final String fanction;
 
   ContactModel(

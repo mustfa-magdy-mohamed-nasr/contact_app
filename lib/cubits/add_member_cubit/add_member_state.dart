f986ac6a384f9c64eddc05d7df7b2c93d20aa1b8
mustfa-magdy-mohamed-nasr <Mostfammaaggddyy@gmail.com>
2024-05-02 06:04:36 +0300

    add add member state

--- lib/cubits/add_member_cubit/add_member_state.dart
+++ lib/cubits/add_member_cubit/add_member_state.dart
@@ -4,3 +4,13 @@ part of 'add_member_cubit.dart';
 sealed class AddMemberState {}
 
 final class AddMemberInitial extends AddMemberState {}
+
+final class AddMemberLoading extends AddMemberState {}
+
+final class AddMemberSuccess extends AddMemberState {}
+
+final class AddMemberFailuer extends AddMemberState {
+  final String errMessadge;
+
+  AddMemberFailuer({required this.errMessadge});
+}

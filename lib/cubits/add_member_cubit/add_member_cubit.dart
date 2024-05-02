61a13fa9f6713f16a92d468ab7957812e1c99baa
mustfa-magdy-mohamed-nasr <Mostfammaaggddyy@gmail.com>
2024-05-02 06:29:29 +0300

    add addMember to add contact cubit

--- lib/cubits/add_member_cubit/add_member_cubit.dart
+++ lib/cubits/add_member_cubit/add_member_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:contact_app/model/contact_model.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
 
 part 'add_member_state.dart';
 
 class AddMemberCubit extends Cubit<AddMemberState> {
   AddMemberCubit() : super(AddMemberInitial());
+
+  addMember(ContactModel member) async {
+    emit(addMember(member));
+    try {
+      var contactBox = Hive.box<ContactModel>('contact');
+      emit(AddMemberSuccess());
+      await contactBox.add(member);
+    } catch (e) {
+      AddMemberFailuer(errMessadge: e.toString());
+      // TODO
+    }
+  }
 }
